3755b2d85c001450f24640d4b4fee9b385e7a77a
Leonardo <llroldan@hotmail.com>
2022-09-08 23:18:43 -0300

    agregamos mock de tareas

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,7 +14,6 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
   toggleAddTask(){
-
   console.log("toggleAddTask");
   }
 }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Terminar el primer modulo angular',
+    day: 'Agosto 5 a las  12:00',
+    reminder: ,
+  },
+  {
+    id: 2,
+    text: 'Terminar el segundomo dulo angular',
+    day: 'Agosto 5 a las  17:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'TInvestifgar sobre TS',
+    day: 'Agosto 5 a las  19:00',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'Leer mi libro',
+    day: 'Agosto 5 a las  21:00',
+    reminder: false,
+  }
+];
